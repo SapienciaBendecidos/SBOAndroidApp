1373caf677f9d3129b3986d68695f53695b8b6c6
Raim <rai.interiano@gmail.com>
2016-12-16 01:28:44 -0600

    New blank slate project

---
+++ app/src/main/java/link/software/nfcapp/MainActivity.java
@@ -0,0 +1,13 @@
+package link.software.nfcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
